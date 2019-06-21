b316c0588ba64299f41308199b45a11b7372c469
Bradley Hennen <bradhennen5@gmail.com>
2019-06-21 18:25:46 -0500

    init

---
+++ src/Components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Grid from './Grid/Grid';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="header">TIC-TAC-TOE</h1>
+      <Grid/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/App/Grid/Grid.js
@@ -0,0 +1,157 @@
+import React, { Component } from 'react';
+// import { connect } from 'react-redux';
+import './Grid.css'
+
+class Grid extends Component {
+
+    state = {
+        flipper: false,
+        boxes: ['topLeft', 'topCenter', 'topRight', 'middleLeft', 'middleCenter', 'middleRight', 'bottomLeft', 'bottomCenter', 'bottomRight'],
+        topLeft: {
+            position: 1,
+            X: false,
+            O: false,
+        },
+        topCenter: {
+            position: 2,
+            X: false,
+            O: false,
+        },
+        topRight: {
+            position: 3,
+            X: false,
+            O: false,
+        },
+        middleLeft: {
+            position: 4,
+            X: false,
+            O: false,
+        },
+        middleCenter: {
+            position: 5,
+            X: false,
+            O: false,
+        },
+        middleRight: {
+            position: 6,
+            X: false,
+            O: false,
+        },
+        bottomLeft: {
+            position: 7,
+            X: false,
+            O: false,
+        },
+        bottomCenter: {
+            position: 8,
+            X: false,
+            O: false,
+        },
+        bottomRight: {
+            position: 9,
+            X: false,
+            O: false,
+        },
+    }
+
+    handleClick = propertyName => (event) => {
+        if (this.state.flipper === false) {
+            this.setState({
+                flipper: !this.state.flipper,
+                [propertyName]: {
+                    ...this.state.propertyName,
+                    X: true,
+                    O: false,
+                }
+            })
+        }
+        else if (this.state.flipper === true) {
+            this.setState({
+                flipper: !this.state.flipper,
+                [propertyName]: {
+                    ...this.state.propertyName,
+                    X: false,
+                    O: true,
+                }
+            })
+        }
+
+    }
+
+    renderSquares = (info, i) => {
+        let state = this.state[info];        
+        if(i%3 === 0 && i !== 0) {
+            if (state.X === false && state.O === false) {
+                return (
+                    < >
+                        <div  className="grid" id={info} onClick={this.handleClick(info)}> `</div>
+                        <br/>
+                    </>
+                )
+            }
+            else if (state.X === true) {
+                return (
+                    < >
+                        <div className="grid" key={i} id={info}><span>X</span></div>
+                        <br/>
+                    </>
+                )
+            }
+            else if (state.O === true) {
+                return (
+                    < >
+                        <div className="grid" key={i} id={info}><span>O</span></div>
+                        <br/>
+                    </>
+                )
+            }
+        }
+        else {
+            if (state.X === false && state.O === false) {
+                return (
+                    <div className="grid" key={i} id={info} onClick={this.handleClick(info)}> `</div>
+                )
+            }
+            else if (state.X === true) {
+                return (
+                    <div className="grid" key={i} id={info}><span>X</span></div>
+                )
+            }
+            else if (state.O === true) {
+                return (
+                    <div className="grid" key={i} id={info}><span>O</span></div>
+                )
+            }
+        }
+    }
+
+    renderXO = () => {
+        return (
+            <div >
+                {/* {JSON.stringify(this.state)} */}
+                <br />
+                {this.state.boxes.map((info, i) => (
+                    this.renderSquares(info, i+1)
+                ))}
+            </div>
+        )
+
+    }
+
+
+    render() {
+
+        return (
+            <div>
+                {this.state.flipper ? <h2>Player Two's Turn</h2> : <h2>Player One's Turn</h2> }
+                <div>
+                    {this.renderXO()}
+                </div>
+            </div>
+        );
+    }
+}
+
+
+
+export default Grid;
